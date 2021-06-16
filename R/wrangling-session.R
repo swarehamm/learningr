c9d3d5e820178e14bd0f37822ed3e332dda90758
Sofia Wareham Mathiassen <sofiawareham@gmail.com>
2021-06-16 09:52:37 +0200

    added instructions

--- R/wrangling-session.R
+++ R/wrangling-session.R
@@ -3,3 +3,16 @@ source(here::here("R/package-loading.R"))
 
 # briefly glimpse content of dataset
 glimpse(NHANES)
+
+#check out colnames
+colnames(NHANES)
+
+#Look at contents
+str()
+glimpse()
+
+#see summary
+summary()
+
+#look over dataset documentation
+?NHANES
